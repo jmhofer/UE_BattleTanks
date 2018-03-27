2ed6f739e7d7e82f93b1f6e23089ca1ad3fa6b63
Joachim Hofer <jmhofer.github@johoop.de>
2018-03-27 13:32:36 +0200

    wire up turning

--- BattleTanks/Source/BattleTanks/Private/TankMovementComponent.cpp
+++ BattleTanks/Source/BattleTanks/Private/TankMovementComponent.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Public/TankMovementComponent.h"
+#include "Public/TankTrack.h"
 
 void UTankMovementComponent::InitialiseComponent(UTankTrack* LeftTrack, UTankTrack* RightTrack) {
 	this->LeftTrack = LeftTrack;
@@ -8,6 +9,19 @@ void UTankMovementComponent::InitialiseComponent(UTankTrack* LeftTrack, UTankTra
 }
 
 void UTankMovementComponent::IntendMoveForward(float Throw) {
-	UE_LOG(LogTemp, Warning, TEXT("intend move forward: %f"), Throw);
-	// TODO
+	if (!LeftTrack || !RightTrack) {
+		return;
+	}
+
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+}
+
+void UTankMovementComponent::IntendTurnRight(float Throw) {
+	if (!LeftTrack || !RightTrack) {
+		return;
+	}
+
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
 }
