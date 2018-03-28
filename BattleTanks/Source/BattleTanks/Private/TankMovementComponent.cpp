e6c381cd0e8d8c275e83e9dd9b71bdef1a45f490
Joachim Hofer <jmhofer.github@johoop.de>
2018-03-28 14:26:59 +0200

    make the tank move forwards, not backwards

--- BattleTanks/Source/BattleTanks/Private/TankMovementComponent.cpp
+++ BattleTanks/Source/BattleTanks/Private/TankMovementComponent.cpp
@@ -16,7 +16,7 @@ void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool
 	auto TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
 
 	IntendMoveForward(FVector::DotProduct(AIForwardIntention, TankForward));
-	IntendTurnRight(FVector::CrossProduct(AIForwardIntention, TankForward).Z);
+	IntendTurnRight(FVector::CrossProduct(TankForward, AIForwardIntention).Z);
 }
 
 void UTankMovementComponent::IntendMoveForward(float Throw) {
