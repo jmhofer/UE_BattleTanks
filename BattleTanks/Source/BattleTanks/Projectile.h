bfc46efd1f38db1df3726d76b9d50b4122337dd2
Joachim Hofer <jmhofer.github@johoop.de>
2018-03-29 12:43:15 +0200

    prepare for particle systems

--- BattleTanks/Source/BattleTanks/Projectile.h
+++ BattleTanks/Source/BattleTanks/Projectile.h
@@ -7,6 +7,8 @@
 #include "Projectile.generated.h"
 
 class UTankProjectileMovementComponent;
+class UStaticMeshComponent;
+class UParticleSystemComponent;
 
 UCLASS()
 class BATTLETANKS_API AProjectile : public AActor
@@ -24,7 +26,14 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* CollisionMesh = nullptr;
+
+	UPROPERTY(VisibleAnywhere)
+	UParticleSystemComponent* LaunchBlast = nullptr;
+
 private:
+
 	UTankProjectileMovementComponent* TankProjectileMovementComponent = nullptr;
 
 	virtual void Tick(float DeltaTime) override;
